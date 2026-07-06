7e09bb6fade08b9b1c7d3c2ed4dc0d0244e4e949
zjxlooou <zjjljh12@163.com>
2026-07-06 11:37:31 +0800

    Update adblockmihomolite URL to new source

--- Script/mihomoScript.js
+++ Script/mihomoScript.js
@@ -512,7 +512,7 @@ const serviceConfigs = [
     providers: {
       adblockmihomolite: {
         ...ruleProviderCommonDomain,
-        url: 'https://fastly.jsdelivr.net/gh/217heidai/adblockfilters@main/rules/adblockmihomolite.mrs',
+        url: 'https://anti-ad.net/mihomo.mrs',
         path: './ruleset/adblockmihomolite.mrs',
         'path-in-bundle': 'geo/geosite/category-ads-all.mrs',
       },
